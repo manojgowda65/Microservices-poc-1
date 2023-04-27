88d4e6dd1a30fd84af85b5ac4641efb2747ccf40
Manoj Gopalakrishna <manojgowdames@gmail.com>
2023-04-27 23:31:52 +0530

    Catalog DB layer added

---
+++ src/Services/Catalog/Catalog.API/Data/CatalogContext.cs
@@ -0,0 +1,22 @@
+﻿using Catalog.API.Entities;
+using MongoDB.Driver;
+
+namespace Catalog.API.Data
+{
+    public class CatalogContext : ICatalogContext
+    {
+        public CatalogContext(IConfiguration config)
+        {
+            var client = new MongoClient(config.GetValue<string>("DataBaseSettings:ConnectionString"));
+            var database = client.GetDatabase(config.GetValue<string>("DataBaseSettings:DataBaseName"));
+
+            Products = database.GetCollection<Product>(config.GetValue<string>("DataBaseSettings:CollectionName"));
+
+            CatalogDataSeed.SeedData(Products);
+        }
+        public IMongoCollection<Product> Products
+        {
+            get;
+        }
+    }
+}
