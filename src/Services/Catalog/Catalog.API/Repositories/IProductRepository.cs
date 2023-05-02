e8d44b9851114b7c523d77a90f4f9baf9696a465
Manoj Gopalakrishna <manojgowdames@gmail.com>
2023-05-02 08:40:35 +0530

    Catalog API implementation & containarize api Mongo DB containeraize, added docker compose

---
+++ src/Services/Catalog/Catalog.API/Repositories/IProductRepository.cs
@@ -0,0 +1,14 @@
+﻿using Catalog.API.Entities;
+
+namespace Catalog.API.Repositories
+{
+    public interface IProductRepository
+    {
+        Task<IList<Product>> GetProducts();
+        Task<Product> GetProductById(string id);
+        Task<IList<Product>> GetProductsByCategory(string category);
+        Task<Product> AddNewProduct(Product product);
+        Task<bool> UpdateProduct(Product product);
+        Task<bool> DeleteProduct(string id);
+    }
+}
---
+++ src/Services/Catalog/Catalog.API/Repositories/ProductRepository.cs
@@ -0,0 +1,53 @@
+﻿using Catalog.API.Data;
+using Catalog.API.Entities;
+using MongoDB.Driver;
+
+namespace Catalog.API.Repositories
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly ICatalogContext _catalogContext;
+        public ProductRepository(ICatalogContext context)
+        {
+            _catalogContext = context??throw new ArgumentNullException(nameof(context));
+        }
+
+        public async Task<Product> AddNewProduct(Product product)
+        {
+            await _catalogContext.Products.InsertOneAsync(product);
+            return product;
+        }
+
+        public async Task<bool> DeleteProduct(string id)
+        {
+            FilterDefinition<Product> filterDefinition = Builders<Product>.Filter.Eq(p => p.Id, id);
+            DeleteResult result = await _catalogContext.Products.DeleteOneAsync(filterDefinition);
+
+            return result.IsAcknowledged && result.DeletedCount > 0;
+        }
+
+        public async Task<IList<Product>> GetProductsByCategory(string category)
+        {
+            FilterDefinition<Product> filterDefinition = Builders<Product>.Filter.Eq(p => p.Category, category);
+            return await _catalogContext.Products.Find(filterDefinition).ToListAsync();
+        }
+
+        public async Task<Product> GetProductById(string id)
+        {
+            return await  _catalogContext.Products.Find(x=>x.Id== id).FirstOrDefaultAsync();
+        }
+
+        public async Task<IList<Product>> GetProducts()
+        {
+            return await _catalogContext.Products.Find(x=>true).ToListAsync();
+        }
+
+        public async Task<bool> UpdateProduct(Product product)
+        {
+            var result = await _catalogContext
+                                        .Products
+                                        .ReplaceOneAsync(filter: p => p.Id==product.Id,replacement: product);
+            return result.IsAcknowledged && result.ModifiedCount > 0;
+        }
+    }
+}
