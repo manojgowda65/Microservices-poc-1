08104580e9593d21561da0816b994f0a24b3a169
Manoj Gopalakrishna <manojgowdames@gmail.com>
2023-05-02 09:59:31 +0530

    Basket API added

---
+++ src/Services/Basket/Basket.API/Entities/ShoppingCart.cs
@@ -0,0 +1,26 @@
+﻿namespace Basket.API.Entities
+{
+    public class ShoppingCart
+    {
+        public string UserName { get; set; }
+        public List<ShoppingCardItem> Items { get; set; } = new List<ShoppingCardItem>();
+        public ShoppingCart() { }
+
+        public ShoppingCart(string userName) {
+            UserName= userName;
+        }
+
+        public decimal TotalPrice
+        {
+            get
+            {
+                decimal totalPrice = 0;
+                foreach (var item in Items)
+                {
+                    totalPrice += item.Price*item.Quantity;
+                }
+                return totalPrice;
+            }
+        }
+    }
+}
